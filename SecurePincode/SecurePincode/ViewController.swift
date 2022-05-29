e3a5053bcb936a0983f91dd61604ebd855d8c8ae
권성우 <swkwon@gwonseong-uui-MacBookPro.local>
2022-05-29 14:57:47 +0900

    intial commit

---
+++ SecurePincode/SecurePincode/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SecurePincode
+//
+//  Created by 권성우 on 2022/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .yellow
+    }
+
+
+}
+
